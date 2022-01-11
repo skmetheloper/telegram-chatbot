9a19f6062f73f42d9f1e318752a2f0172302486c
Shin Maung Maung <ethereal97@gmail.com>
2022-01-12 02:29:58 +0630

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,15 +1,30 @@
 const { database } = require('../lib/firebase');
+const { verify, handle} = require('../lib/telegram');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-
-});
-
 app.post('/', (req, res) => {
+  const { token } = req.query;
+  const { message } = req.body;
 
+  if (!verify(token)) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  if (typeof message !== 'object') {
+    return res.status(400).send("Bad Request");
+  }
+
+  switch(message.chat.type) {
+    case 'private':
+      handle.message(message);
+      break;
+    }
+  }
+  
+  res.send('OK');
 });
 
 module.exports = app;
